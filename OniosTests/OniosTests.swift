a2cd8198f94fb2f371e38c3a7ceac1765874424d
Pablo Zirilli <138540130+PolZirilli@users.noreply.github.com>
2025-06-07 03:07:53 -0300

    Add files via upload

---
+++ OniosTests/OniosTests.swift
@@ -0,0 +1,17 @@
+//
+//  OniosTests.swift
+//  OniosTests
+//
+//  Created by Catalina Picco on 06/06/2025.
+//
+
+import Testing
+@testable import Onios
+
+struct OniosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
